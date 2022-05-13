7cd9a93bece04b04b960d7580e7d6cbe632b80c9
amexiasd <amexias@outlook.com>
2022-05-13 16:30:01 +0300

    player

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-console.log('OK');
 
 function computerPlay() {
 	let plays = ['Rock', 'Paper', 'Scissors'];
 	let rand = Math.floor(Math.random() * plays.length);
-	console.log(rand);
 	return plays[rand];
 }
 
+function readplayer() {
+let answer = prompt("Enter play:\n 1.Rock \n 2.Paper \n 3.Scissors \n");
+console.log(answer);
+}
+
 let cplay = computerPlay();
+readplayer();
 console.log(cplay);
